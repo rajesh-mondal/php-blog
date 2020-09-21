0b06b313a2f6da7d616446c6c299ec955b94a8a6
Rajesh Mondal <rajesh01749@gmail.com>
2020-09-21 18:27:13 +0600

    admin panel modified

--- admin/all-categories.php
+++ admin/all-categories.php
@@ -3,7 +3,7 @@
   <div class="container-fluid">
 
     <!-- Page Heading -->
-    <h1 class="h3 mb-4 text-gray-800">Blank Page</h1>
+    <h1 class="h3 mb-4 text-gray-800">View All Categories</h1>
 
     </div>
     <!-- /.container-fluid -->
