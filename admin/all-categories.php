43839e300002d155fd1ceea3973abce9946bed12
Rajesh Mondal <rajesh01749@gmail.com>
2020-09-25 20:29:35 +0600

    category update done

--- admin/all-categories.php
+++ admin/all-categories.php
@@ -15,7 +15,7 @@
       $addCategory = mysqli_query($connect, $query);
 
       if(!$addCategory){
-        die("Query Failed" . mysqli_error($connect));
+        die("Query Failed " . mysqli_error($connect));
       }
       else{
         $message = '<div class="alert alert-success">Category Name Added Successfully.</div>';
@@ -47,7 +47,7 @@
             <form action="" method="POST">
               <div class="form-group">
                 <label for="Category">Add Category Name</label>
-                <input type="text" name="category" class="form-control" autocomplete="off">
+                <input type="text" name="category" class="form-control" required="required" autocomplete="off">
               </div>
               <div class="form-group">
                 <input type="submit" name="addCategory" value="Add Category" class="btn btn-primary">
@@ -59,6 +59,45 @@
             <!-- Category Form End -->
           </div>
         </div>
+
+        <?php
+          if(isset($_GET['update'])){ 
+            $cat_id = $_GET['update'];
+            
+            $query = "SELECT * FROM categories WHERE cat_id = $cat_id";
+
+            $select_category_id = mysqli_query($connect, $query);
+
+            while($row = mysqli_fetch_assoc($select_category_id)){
+              
+              $cat_id     = $row['cat_id'];
+              $cat_name   = $row['cat_name'];
+
+              ?>
+
+              <div class="card shadow mb-4">
+                <div class="card-header py-3">
+                  <h6 class="m-0 font-weight-bold text-primary">Edit Category</h6>
+                </div>
+                <div class="card-body">
+                  <!-- Category Form Start -->
+                  <form action="" method="POST">
+                    <div class="form-group">
+                      <label for="Category">Edit Category Name</label>
+                      <input type="text" name="category" class="form-control" required="required" autocomplete="off" value="<?php if(isset($cat_id)){ echo $cat_name; } ?>">
+                    </div>
+                    <div class="form-group">
+                      <input type="submit" name="editCategory" value="Update Category" class="btn btn-primary">
+                    </div>
+                  </form>
+                  <!-- Category Form End -->
+                </div>
+              </div>
+
+        <?php } 
+            }
+        ?>
+
       </div>
       <!-- Add New Category Field End -->
 
@@ -94,7 +133,7 @@
                       <td><?php echo $cat_name; ?></td>
                       <td>
                         <div class="btn-group">
-                          <a href="" class="btn btn-primary btn-sm">Update</a>
+                          <a href="all-categories.php?update=<?php echo $cat_id; ?>" class="btn btn-primary btn-sm">Update</a>
                           <a href="" class="btn btn-danger btn-sm">Delete</a>
                         </div>
                       </td>
@@ -115,5 +154,24 @@
 
   </div>
   <!-- End of Main Content -->
+
+<?php
+
+  if(isset($_POST['editCategory'])){
+    $cat_name = $_POST['category'];
+
+    $query = "UPDATE categories SET cat_name='$cat_name' WHERE cat_id='$cat_id'";
+
+    $update_category = mysqli_query($connect, $query);
+
+    if(!$update_category){
+      die("Query Failed " . mysqli_error($connect));
+    }
+    else{
+      header("Location: all-categories.php");
+    }
+  }
+
+?>
 
 <?php include "includes/footer.php"?>
