8a5f9a37bc5c74ecf4fe98469043dd1719dba4f4
Rajesh Mondal <rajesh01749@gmail.com>
2020-09-28 14:08:29 +0600

    comment line added and space removed

--- admin/all-categories.php
+++ admin/all-categories.php
@@ -3,17 +3,15 @@
 <?php
   $message = "";
 
+  // Add New Category Codes are Here
   if(isset($_POST['addCategory'])){
     $cat_name = $_POST['category'];
-
     if(empty($cat_name)){
       $message = '<div class="alert alert-warning">Category Name Can not be Empty.</div>';
     }
     else{
       $query = "INSERT INTO categories (cat_name) VALUES ('$cat_name')";
-
       $addCategory = mysqli_query($connect, $query);
-
       if(!$addCategory){
         die("Query Failed " . mysqli_error($connect));
       }
@@ -27,7 +25,6 @@
 
   <!-- Begin Page Content -->
   <div class="container-fluid">
-
     <div class="row">
       <div class="col-lg-12">
         <!-- Page Heading -->
@@ -59,20 +56,18 @@
             <!-- Category Form End -->
           </div>
         </div>
+        <!-- Add New Category Field End -->
 
+        <!-- Update Category Field Start -->
         <?php
+          // If Category Needs to get Update
           if(isset($_GET['update'])){ 
-            $cat_id = $_GET['update'];
-            
+            $cat_id = $_GET['update'];           
             $query = "SELECT * FROM categories WHERE cat_id = $cat_id";
-
             $select_category_id = mysqli_query($connect, $query);
-
-            while($row = mysqli_fetch_assoc($select_category_id)){
-              
+            while($row = mysqli_fetch_assoc($select_category_id)){              
               $cat_id     = $row['cat_id'];
               $cat_name   = $row['cat_name'];
-
               ?>
 
               <div class="card shadow mb-4">
@@ -93,13 +88,11 @@
                   <!-- Category Form End -->
                 </div>
               </div>
-
         <?php } 
             }
         ?>
-
+        <!-- Update Category Field End -->
       </div>
-      <!-- Add New Category Field End -->
 
       <!-- View All Category list Start -->
       <div class="col-lg-8">
@@ -119,7 +112,9 @@
                 </tr>
               </thead>
               <tbody>
+
                 <?php
+                  // View All Categories Codes are Here
                   $query = "SELECT * FROM categories";
                   $select_categories = mysqli_query( $connect, $query );
 
@@ -128,8 +123,8 @@
                     $i++;
                     $cat_id   = $row['cat_id'];
                     $cat_name = $row['cat_name'];
-
                     ?>
+
                     <tr>
                       <td><?php echo $i; ?></td>
                       <td><?php echo $cat_name; ?></td>
@@ -158,16 +153,29 @@
   </div>
   <!-- End of Main Content -->
 
-<?php
+  <?php
+    // Update Category
+    if(isset($_POST['editCategory'])){
+      $cat_name = $_POST['category'];
+      $query = "UPDATE categories SET cat_name='$cat_name' WHERE cat_id='$cat_id'";
+      $update_category = mysqli_query($connect, $query);
+      if(!$update_category){
+        die("Query Failed " . mysqli_error($connect));
+      }
+      else{
+        header("Location: all-categories.php");
+      }
+    }
 
-  if(isset($_POST['editCategory'])){
-    $cat_name = $_POST['category'];
+  ?>
 
-    $query = "UPDATE categories SET cat_name='$cat_name' WHERE cat_id='$cat_id'";
-
-    $update_category = mysqli_query($connect, $query);
-
-    if(!$update_category){
+  <?php
+  // Delete Category from Database
+  if(isset($_GET['delete'])){
+    $the_cat_id = $_GET['delete'];
+    $query = "DELETE FROM categories WHERE cat_id=$the_cat_id";
+    $delete_category = mysqli_query($connect, $query);
+    if(!$delete_category){
       die("Query Failed " . mysqli_error($connect));
     }
     else{
@@ -175,26 +183,7 @@
     }
   }
 
-?>
-
-<?php
-// Delete Category from Database
-if(isset($_GET['delete'])){
-  $the_cat_id = $_GET['delete'];
-
-  $query = "DELETE FROM categories WHERE cat_id=$the_cat_id";
-
-  $delete_category = mysqli_query($connect, $query);
-
-  if(!$delete_category){
-    die("Query Failed " . mysqli_error($connect));
-  }
-  else{
-    header("Location: all-categories.php");
-  }
-}
-
-?>
+  ?>
 
 
 <?php include "includes/footer.php"?>
