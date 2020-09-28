de347d4d34a23030562466ab3b65056ce93fe4e9
Rajesh Mondal <rajesh01749@gmail.com>
2020-09-28 18:00:27 +0600

    category page re-organized

--- admin/all-categories.php
+++ admin/all-categories.php
@@ -1,26 +1,9 @@
 <?php include "includes/header.php"?>
 
+<!--- Add New Category on the Category Page --->
 <?php
-  $message = "";
-
-  // Add New Category Codes are Here
-  if(isset($_POST['addCategory'])){
-    $cat_name = $_POST['category'];
-    if(empty($cat_name)){
-      $message = '<div class="alert alert-warning">Category Name Can not be Empty.</div>';
-    }
-    else{
-      $query = "INSERT INTO categories (cat_name) VALUES ('$cat_name')";
-      $addCategory = mysqli_query($connect, $query);
-      if(!$addCategory){
-        die("Query Failed " . mysqli_error($connect));
-      }
-      else{
-        $message = '<div class="alert alert-success">Category Name Added Successfully.</div>';
-        header("Location: all-categories.php");
-      }
-    }
-  }
+  //$message = "";
+  insert_category();
 ?>
 
   <!-- Begin Page Content -->
@@ -51,7 +34,7 @@
               </div>
             </form>
             <?php
-              echo $message;
+              //echo $message;
             ?>
             <!-- Category Form End -->
           </div>
@@ -115,30 +98,7 @@
 
                 <?php
                   // View All Categories Codes are Here
-                  $query = "SELECT * FROM categories";
-                  $select_categories = mysqli_query( $connect, $query );
-
-                  $i = 0;
-                  while ($row = mysqli_fetch_assoc($select_categories)){
-                    $i++;
-                    $cat_id   = $row['cat_id'];
-                    $cat_name = $row['cat_name'];
-                    ?>
-
-                    <tr>
-                      <td><?php echo $i; ?></td>
-                      <td><?php echo $cat_name; ?></td>
-                      <th scope="row"><?php echo $cat_id; ?></th>
-                      <td>
-                        <div class="btn-group">
-                          <a href="all-categories.php?update=<?php echo $cat_id; ?>" class="btn btn-primary btn-sm">Update</a>
-                          <a href="all-categories.php?delete=<?php echo $cat_id; ?>" class="btn btn-danger btn-sm">Delete</a>
-                        </div>
-                      </td>
-                    </tr>
-
-                    <?php
-                  }
+                  view_all_category();
                 ?>
               </tbody>
             </table>
@@ -169,20 +129,9 @@
 
   ?>
 
+  <!--- Delete Category From the Database --->
   <?php
-  // Delete Category from Database
-  if(isset($_GET['delete'])){
-    $the_cat_id = $_GET['delete'];
-    $query = "DELETE FROM categories WHERE cat_id=$the_cat_id";
-    $delete_category = mysqli_query($connect, $query);
-    if(!$delete_category){
-      die("Query Failed " . mysqli_error($connect));
-    }
-    else{
-      header("Location: all-categories.php");
-    }
-  }
-
+    delete_category();
   ?>
 
 
