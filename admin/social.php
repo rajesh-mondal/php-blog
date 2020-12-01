929b3222aef7626c426e936150792dcd2f00bb78
Rajesh Mondal <rajesh01749@gmail.com>
2020-12-01 23:39:55 +0600

    social media linkup page html markup done

---
+++ admin/social.php
@@ -0,0 +1,80 @@
+<?php include "includes/header.php"?>
+  <!-- Begin Page Content -->
+  <div class="container-fluid">
+
+    <!-- Page Heading -->
+    <h1 class="h3 mb-4 text-gray-800">Blank Page</h1>
+
+    <div class="row">
+        <div class="col-md-12">
+            <div class="card shadow mb-4">
+                <div class="card-header py-3">
+                    <h6 class="m-0 font-weight-bold text-primary">Manage Social Media URL's</h6>
+                </div>
+                <div class="card-body">
+                    <form action="" method="POST">
+                        <div class="form-group row">
+                            <label class="col-sm-2 col-form-label">Facebook</label>
+                            <div class="col-sm-8">
+                                <input type="text" class="form-control" placeholder="Facebook URL">
+                            </div>
+                            <div class="col-sm-2">
+                                <input type="submit" name="facebook-save" value="Save" class="btn btn-primary btn-flat btn-sm" >
+                            </div>
+                        </div>
+                    </form>
+                    <form action="" method="POST">
+                        <div class="form-group row">
+                            <label class="col-sm-2 col-form-label">Twitter</label>
+                            <div class="col-sm-8">
+                                <input type="text" class="form-control" placeholder="Twitter URL">
+                            </div>
+                            <div class="col-sm-2">
+                                <input type="submit" name="twitter-save" value="Save" class="btn btn-primary btn-flat btn-sm" >
+                            </div>
+                        </div>
+                    </form>
+                    <form action="" method="POST">
+                        <div class="form-group row">
+                            <label class="col-sm-2 col-form-label">Linkedin</label>
+                            <div class="col-sm-8">
+                                <input type="text" class="form-control" placeholder="Linkedin URL">
+                            </div>
+                            <div class="col-sm-2">
+                                <input type="submit" name="linkedin-save" value="Save" class="btn btn-primary btn-flat btn-sm" >
+                            </div>
+                        </div>
+                    </form>
+                    <form action="" method="POST">
+                        <div class="form-group row">
+                            <label class="col-sm-2 col-form-label">Youtube</label>
+                            <div class="col-sm-8">
+                                <input type="text" class="form-control" placeholder="Youtube URL">
+                            </div>
+                            <div class="col-sm-2">
+                                <input type="submit" name="youtube-save" value="Save" class="btn btn-primary btn-flat btn-sm" >
+                            </div>
+                        </div>
+                    </form>
+                    <form action="" method="POST">
+                        <div class="form-group row">
+                            <label class="col-sm-2 col-form-label">Behance</label>
+                            <div class="col-sm-8">
+                                <input type="text" class="form-control" placeholder="Behance URL">
+                            </div>
+                            <div class="col-sm-2">
+                                <input type="submit" name="behance-save" value="Save" class="btn btn-primary btn-flat btn-sm" >
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    </div>
+    <!-- /.container-fluid -->
+
+  </div>
+  <!-- End of Main Content -->
+<?php include "includes/footer.php"?>
